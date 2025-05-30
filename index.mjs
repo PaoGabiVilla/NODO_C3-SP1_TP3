67c4f15f2eac84dd1fba8ccda9e139bd99810d80
PaoGabiVilla <pagavi161179@gmail.com>
2025-05-30 18:47:18 -0300

    Modificación de errores

--- index.mjs
+++ index.mjs
@@ -1,14 +1,13 @@
-import { leerSuperheroes, agregarSuperheroes } from './utils.mjs';   
+import { leerSuperheroes, agregarSuperheroes } from './utils.mjs';
 
-    const archivoOriginal = './superheroes.txt';
-    const archivoNuevos = './agregarSuperheroes.txt';
+const archivoOriginal = './superheroes.txt';
+const archivoNuevos = './agregarSuperheroes.txt';
 
-    // Primero agregamos los nuevos superhéroes
-    agregarSuperheroes(archivoOriginal, archivoNuevos);
+// Agregar nuevos superhéroes
+agregarSuperheroes(archivoOriginal, archivoNuevos);
 
-    // Leer y mostrar la lista de superhéroes ordenada
-    const superheroes = leerSuperheroes(archivoOriginal); 
-    console.log('Superhéroes ordenados:');
-    console.log(superheroes);
+// Leer y mostrar la lista actualizada de superhéroes ordenada
+const superheroes = leerSuperheroes(archivoOriginal);
 
-
+console.log('Superhéroes ordenados:');
+console.log(superheroes);
